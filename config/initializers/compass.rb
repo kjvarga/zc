0abae43af95a1c5f4e04b0e15f51c0350f783616
Karl Varga <kjvarga@gmail.com>
2011-04-12 11:53:39 -0700

    Try a Compass config that works with Rails 3.0.5...didn't work * Move the SASS files out so it doesn't find them

--- config/initializers/compass.rb
+++ config/initializers/compass.rb
@@ -1,5 +1,18 @@
 require 'compass'
-# If you have any compass plugins, require them here.
-Compass.add_project_configuration(File.join(RAILS_ROOT, "config", "compass.config"))
-Compass.configuration.environment = RAILS_ENV.to_sym
-Compass.configure_sass_plugin!
+require 'compass/app_integration/rails'
+Compass::AppIntegration::Rails.initialize!
+
+require 'fileutils'
+FileUtils.mkdir_p(Rails.root.join("tmp", "stylesheets"))
+
+environment = Compass::AppIntegration::Rails.env
+if environment == 'production'
+  Compass::AppIntegration::Rails.initialize!
+
+  Rails.configuration.middleware.delete('Sass::Plugin::Rack')
+  Rails.configuration.middleware.insert_before('Rack::Sendfile', 'Sass::Plugin::Rack')
+
+  Rails.configuration.middleware.insert_before('Rack::Sendfile', 'Rack::Static',
+      :urls => ['/stylesheets'],
+      :root => "#{Rails.root}/tmp")
+end
