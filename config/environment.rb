7ffe9451be4ed30e86df04dc89369aa74e028966
Karl Varga <kjvarga@gmail.com>
2011-04-12 12:06:51 -0700

    Use a Gemfile

--- config/environment.rb
+++ config/environment.rb
@@ -11,14 +11,5 @@ Rails::Initializer.run do |config|
   # Add additional load paths for your own custom dirs
   # config.load_paths += %W( #{RAILS_ROOT}/extras )
 
-  config.gem "haml"
-  config.gem 'compass'
-  config.gem 'will_paginate'
-  config.gem 'authlogic'
-  config.gem 'searchlogic'
-  config.gem 'acts-as-taggable-on'
-  config.gem 'machinist'
-  config.gem 'acts_as_commentable'
-
   config.time_zone = 'UTC'
 end
