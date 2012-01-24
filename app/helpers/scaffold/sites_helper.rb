b08b36ad43fa98e12b4b90f86fd62158130e3b4d
Karl Varga <kjvarga@gmail.com>
2012-01-23 19:18:55 -0800

    Start fresh.

--- app/helpers/scaffold/sites_helper.rb
+++
@@ -1,7 +0,0 @@
-module Scaffold
-  module SitesHelper
-    def posts_column(record)
-      record.posts.count
-    end
-  end
-end
--- config/initializers/exception_notification.rb
+++
@@ -1,6 +0,0 @@
-begin
-  ExceptionNotifier.sender_address =  %("error@#{SiteDefaults.HOST}")
-  ExceptionNotifier.exception_recipients = [SiteDefaults.ADMIN_EMAIL]
-  ExceptionNotifier.email_prefix = "[#{SiteDefaults.NAME}] "
-rescue
-end
