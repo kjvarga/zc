b586af51bfe9ef0a797151cd08453a939bfb2bae
Karl Varga <kjvarga@gmail.com>
2012-01-23 19:28:14 -0800

    Clean 3.2.0 install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ziecreative::Application.config.secret_token = '035323412d5f695673ad96595005903b189310da2b3024d70c34df395bb5d857b38d4514576708aca095028a635103595ad4d86c9f13ec74407aef6aeec1ee0d'
